a9e1d8f274b406a58e4e6862522ae22595b012bf
Rebecca Pearce <becciepearce@hotmail.co.uk>
2016-09-01 20:01:22 +0100

    adds tests for tables

--- spec/features/confidences_feature_spec.rb
+++ spec/features/confidences_feature_spec.rb
@@ -4,22 +4,22 @@ require 'web_helper'
 feature 'Confidences' do
   before do
     sign_up
+    add_confidences
   end
 
-  context 'skills and initial confidence levels have been set' do
-    scenario 'a user sets daily confidence levels' do
-      visit '/'
-      click_link 'Set confidence'
-      within(:css, "#Ruby") do
-        select '5', from: 'feedback__rating'
-      end
-      within(:css, "#Javascript") do
-        select '7', from: 'feedback__rating'
-      end
-      click_button 'Submit'
-      expect(current_path).to eq '/skills'
+  context 'viewing the graph' do
+    scenario 'user has added day 1 confidences' do
+      visit '/skills'
       page.has_css? 'g.highcharts-grid'
+      page.has_css? 'highcharts-legend-item'
     end
   end
-
+  context 'viewing the table' do
+    scenario 'user has added day 1 confidences' do
+      visit '/skills'
+      click_link 'View Data Table'
+      expect(page).to have_content 'Ruby 5'
+      expect(page).to have_content 'Javascript 7'
+    end
+  end
 end
