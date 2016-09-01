e894f8a54c062eba31f09c597dc0163360648f0b
Rebecca Pearce <becciepearce@hotmail.co.uk>
2016-09-01 12:17:05 +0100

    works on putting array in db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160830184117) do
+ActiveRecord::Schema.define(version: 20160901103302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,11 @@ ActiveRecord::Schema.define(version: 20160830184117) do
     t.integer  "skill_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "links", force: :cascade do |t|
+    t.string "name"
+    t.text   "url_resources", default: [], array: true
   end
 
   create_table "skills", force: :cascade do |t|
