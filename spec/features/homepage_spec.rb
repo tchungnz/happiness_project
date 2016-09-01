eab935e3aaf4c6022fb091579688c5501a64f7ec
Rebecca Pearce <becciepearce@hotmail.co.uk>
2016-09-01 18:36:06 +0100

    testing things soTDD

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 feature 'Homepage' do
   context 'user is not signed up/in' do
     scenario 'should display a welcome message' do
-      visit '/skills'
+      visit '/'
       expect(page).to have_content 'BLOOM.'
     end
   end
