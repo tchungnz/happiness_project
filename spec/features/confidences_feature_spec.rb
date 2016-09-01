ae0cd2d1f1d46ed3c4c3ee1c0664e54d0b91049e
Rebecca Pearce <becciepearce@hotmail.co.uk>
2016-09-01 18:50:46 +0100

    fixes more tests

--- spec/features/confidences_feature_spec.rb
+++ spec/features/confidences_feature_spec.rb
@@ -8,7 +8,7 @@ feature 'Confidences' do
 
   context 'skills and initial confidence levels have been set' do
     scenario 'a user sets daily confidence levels' do
-      visit '/skills'
+      visit '/'
       click_link 'Set confidence'
       within(:css, "#Ruby") do
         select '5', from: 'feedback__rating'
@@ -18,8 +18,7 @@ feature 'Confidences' do
       end
       click_button 'Submit'
       expect(current_path).to eq '/skills'
-      expect(page).to have_content '5'
-      expect(page).to have_content '7'
+      page.has_css? 'g.highcharts-grid'
     end
   end
 
