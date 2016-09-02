5809ed811a92365d60523839d2d750f3fcb32b14
Rebecca Pearce <becciepearce@hotmail.co.uk>
2016-09-02 09:42:16 +0100

    tests for seven days set up

--- spec/features/confidences_feature_spec.rb
+++ spec/features/confidences_feature_spec.rb
@@ -7,26 +7,46 @@ feature 'Confidences' do
     add_confidences
   end
 
-  context 'viewing the graph' do
-    scenario 'user has added day 1 confidences' do
+  context 'user has added day 1 confidences' do
+    scenario 'viewing the graph' do
       visit '/skills'
       page.has_css? 'g.highcharts-grid'
       page.has_css? 'highcharts-legend-item'
     end
-  end
-  context 'viewing the table' do
-    scenario 'user has added day 1 confidences' do
+    scenario 'viewing the table' do
       visit '/skills'
       click_link 'View Data Table'
       expect(page).to have_content 'Ruby 5'
       expect(page).to have_content 'Javascript 7'
     end
+    scenario 'viewing individual graphs' do
+      visit '/skills'
+      click_link 'View Data Table'
+      click_link 'Ruby'
+      page.has_css? 'g.highcharts-grid'
+    end
   end
-  scenario 'viewing individual graphs' do
-    visit '/skills'
-    click_link 'View Data Table'
-    click_link 'Ruby'
-    page.has_css? 'g.highcharts-grid'
+  before do 
+    seven_confidences
+  end
+  context 'user has added 7 days of confidences' do
+    scenario 'viewing the graph' do
+      visit '/skills'
+      page.has_css? 'g.highcharts-grid'
+      page.has_css? 'highcharts-legend-item'
+    end
+    scenario 'viewing the table' do
+      visit '/skills'
+      click_link 'View Data Table'
+      expect(page).to have_content 'Ruby 5'
+      expect(page).to have_content 'Javascript 7'
+    end
+    scenario 'user has added 7 days of confidences' do
+      visit '/skills'
+      click_link 'View Data Table'
+      click_link 'Ruby'
+      page.has_css? 'g.highcharts-grid'
+    end
   end
   
 end
