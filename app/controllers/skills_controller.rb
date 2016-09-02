471bd7335b8e7ea47a7355d56533e3afeecaa125
Rebecca Pearce <becciepearce@hotmail.co.uk>
2016-09-02 10:16:35 +0100

    Refactor controllers

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -28,12 +28,9 @@ class SkillsController < ApplicationController
       redirect_to '/confidences/new'
     else
     @skill_names = ["Ruby", "Javascript", "HTML", "CSS", "Command Line", "Communication", "Project Work", "TDD", "Agile", "OOP", "Rails", "Databases", "ORM", "jQuery", "Version Control"]
-    @skills = []
-    @confidences = []
     i = 0
     15.times do
-      @skills << Skill.create(name: @skill_names[i], user_id: current_user.id)
-      @confidences << Confidence.new
+      Skill.create(name: @skill_names[i], user_id: current_user.id)
       i += 1
     end
     redirect_to '/confidences/new'
@@ -52,28 +49,9 @@ class SkillsController < ApplicationController
         @skilldata << confidence.rating
       end
       @skillsdata = @skilldata.last(7)
-      language_grouping
   end
 
   private
-
-  def grouping
-    @skill_names = ["Ruby", "Javascript", "HTML", "CSS", "Command Line", "Communication", "Project Work", "TDD", "Agile", "OOP", "Rails", "Databases", "ORM", "jQuery", "Version Control"]
-    @language_data = @skill_names.first(5)
-    @soft_data = @skill_names[6, 7]
-    @meth_data = @skill_names[8..10]
-    @tools_data = @skill_names[11..15]
-  end
-
-  def language_grouping
-    grouping
-    @language_skill = Skill.find(params[:id])
-    @language_ratings = []
-    @language_skill.confidences.each do |confidence|
-      @language_ratings << confidence.rating
-    end
-    @language_ratings_data = @language_ratings.last(7)
-  end
 
   def skills_params
     params.require(:skill).permit(:name)
